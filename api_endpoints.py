611d979aa77a1a5a2649ec56291b49f7e3e86aaa
Conan Chan <chanconan67@gmail.com>
2021-05-09 09:40:32 -0700

    moved new column creation outside of function, added endpoint for getting single property info

--- api_endpoints.py
+++ api_endpoints.py
@@ -5,16 +5,26 @@ import json
 
 app = Flask(__name__)
 api = Api(app)
+data = pd.read_csv('properties.csv')
+data['UPVOTE'] = 0
+data['DOWNVOTE'] = 0
 
 class Properties(Resource):
     def get(self):
-        data = pd.read_csv('properties.csv')
-        data['UPVOTE'] = 0
-        data['DOWNVOTE'] = 0
-        data = data.sort_values(by=["DAYS ON MARKET"], ascending=True)
-        data = data.to_json(orient="index")
-        return data, 200
+        allProperties = data.sort_values(by=["DAYS ON MARKET"], ascending=True)
+        allProperties = allProperties.to_json(orient="index")
+        return allProperties, 200
+
+class PropertyById(Resource):
+    def get(self, id):
+        singleProperty = data.to_dict()
+        prop = {}
+        for field in singleProperty:
+            prop[field] = singleProperty[field][id]
+        return {id:prop}, 200
 
 api.add_resource(Properties, '/properties')
+api.add_resource(PropertyById, '/properties/<int:id>')
+
 if __name__ == '__main__':
     app.run()  # run our Flask app
